c8fa2658604b2247a526dbdb76d3fa4fa8daa2f8
John Slabbekoorn <john.slabbekoorn@gmail.com>
2013-03-25 21:51:59 +0100

    New

---
+++ mr_Exercise2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mr_Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intRegel;
+
+            Console.WriteLine("Geef 5 zinnen in. \n");
+
+            string[] stringArray = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Zin" + (i + 1) + ": ");
+                stringArray[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Welke regel wil je oprvragen?");
+            intRegel = Int32.Parse(Console.ReadLine());
+            intRegel = intRegel - 1;
+
+            Console.WriteLine(stringArray[intRegel]);
+            Console.ReadLine();
+        }
+    }
+}
